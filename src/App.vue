968a6098061e97854a4149ffb5e8fe236ebf32fd
Artur Stahov <vtemy.at.ua@gmail.com>
2021-04-24 13:43:04 +0300

    v1.0

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+   <StarRaitsng :raiting="4"/>
+  </div>
+</template>
+
+<script>
+import StarRaitsng from './components/StarRaiting'
+
+export default {
+  name: 'App',
+  components: {
+ StarRaitsng
+  }
+}
+</script>
+
+<style>
+
+</style>
